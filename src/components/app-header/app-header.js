3f36591b65868d882ecfb42b1cdf02c46aa5411a
vlad <bbladik12@gmail.com>
2022-01-03 19:52:51 +0200

    refactoring code + create ui without any logic inside and created structure of app

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AppHeader = () => {
+    return (
+        <div className='app-header d-flex'>
+            <h1>Vlad Bugaets</h1>
+            <h2>20 записей, 5 лайков</h2>
+        </div>
+    )
+}
+
+export default AppHeader
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SearchPanel = () => {
+    return (
+        <div>
+            <input
+                type="text"
+                className='form-control search-input'
+                placeholder='Search'
+            />
+        </div>
+    )
+}
+
+export default SearchPanel;
